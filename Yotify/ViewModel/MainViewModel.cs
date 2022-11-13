9eb6f5cd1208313ba2633a0b034c161a99f79779
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2022-11-13 16:53:58 +0100

    Add project files.

---
+++ Yotify/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yotify.Core;
+
+namespace Yotify.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand PlaylistsViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public PlaylistsViewModel PlaylistsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { 
+                return _currentView;
+            }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Debug.WriteLine("Init main viewmodel");
+
+            HomeVM = new HomeViewModel();
+            PlaylistsVM = new PlaylistsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                Debug.WriteLine("set home as current");
+                CurrentView = HomeVM;
+            });
+
+            PlaylistsViewCommand = new RelayCommand(o =>
+            {
+                Debug.WriteLine("set playlists as current");
+                CurrentView = PlaylistsVM;
+            });
+        }
+    }
+}
