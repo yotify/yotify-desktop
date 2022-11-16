5455271b77d35aeebca7336faa3f3537691b7cfc
tdrebes <38798057+tdrebes@users.noreply.github.com>
2022-11-16 11:49:05 +0100

    Create youtube playlist model

---
+++ Yotify/Data/Model/Playlist/YoutubePlaylist.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Yotify.Data.Model.MediaItem;
+
+namespace Yotify.Data.Model.Playlist
+{
+    internal class YoutubePlaylist : IPlaylist
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PlaylistURL { get; set; }
+        public string ThumbnailURL { get; set; }
+        public List<IMediaItem> MediaItems { get; set; }
+
+        public YoutubePlaylist(string name, string description, string playlistURL, string thumbnailURL, List<IMediaItem>? mediaItems = null)
+        {
+            Name = name;
+            Description = description;
+            PlaylistURL = playlistURL;
+            ThumbnailURL = thumbnailURL;
+            MediaItems = mediaItems ?? new List<IMediaItem>();
+        }
+
+        public void AddMediaItem(IMediaItem mediaItem)
+        {
+            MediaItems.Add(mediaItem);
+        }
+    }
+}
