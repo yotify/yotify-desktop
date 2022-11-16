e66bea05e7d9f73728cef66eac995e8f08525756
tdrebes <38798057+tdrebes@users.noreply.github.com>
2022-11-16 11:27:30 +0100

    Create model interfaces

---
+++ Yotify/Data/Model/Playlist/IPlaylist.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Yotify.Data.Model.MediaItem;
+
+namespace Yotify.Data.Model.Playlist
+{
+    internal interface IPlaylist
+    {
+        string Name { get; set; }
+
+        string Description { get; set; }
+
+        string PlaylistURL { get; set; }
+
+        string ThumbnailURL { get; set; }
+
+        List<IMediaItem> MediaItems { get; set; }
+    }
+}
