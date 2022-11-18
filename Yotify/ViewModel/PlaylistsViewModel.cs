b87db5cd6a4f6a8feba8ed7299069b179db80b89
tdrebes <38798057+tdrebes@users.noreply.github.com>
2022-11-18 11:01:13 +0100

    Playlist binding

--- Yotify/ViewModel/PlaylistsViewModel.cs
+++ Yotify/ViewModel/PlaylistsViewModel.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Yotify.Core;
+using Yotify.Data.Model.Playlist;
 
 namespace Yotify.ViewModel
 {
     internal class PlaylistsViewModel: ObservableObject
     {
+        public List<IPlaylist> Playlists { get; set; }
+
+        public PlaylistsViewModel()
+        {
+            Playlists = new List<IPlaylist>
+            {
+                new YoutubePlaylist("Playlist 1", "Very nice music", "https://3b34b435b345b.test", ""),
+                new YoutubePlaylist("Playlist 2", "Very nice music", "https://3b34b435b345b.test", ""),
+                new YoutubePlaylist("Playlist 3", "Very nice music", "https://3b34b435b345b.test", ""),
+                new YoutubePlaylist("Playlist 4", "Very nice music", "https://3b34b435b345b.test", ""),
+            };
+        }
     }
 }
