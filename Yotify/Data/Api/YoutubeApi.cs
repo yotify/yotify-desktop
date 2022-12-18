5f3f98c3f99f51d10e441e517cedccad0c18520d
tdrebes <38798057+tdrebes@users.noreply.github.com>
2022-12-18 13:37:28 +0100

    Add YoutubeApi

---
+++ Yotify/Data/Api/YoutubeApi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Yotify.Data.Model.Playlist;
+
+namespace Yotify.Data.Api
+{
+    internal class YoutubeApi
+    {
+        public HttpClient? Client { get; private set; }
+
+        public async Task<string> Search()
+        {
+            string response = await GetClient().GetStringAsync("search");
+
+            return response;
+        }
+
+        public async Task<List<IPlaylist>> GetPlaylistsForCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<IPlaylist>> GetPlaylistsForUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private HttpClient GetClient()
+        {
+            if (Client != null)
+            {
+                return Client;
+            }
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://youtube.googleapis.com/youtube/v3/");
+            client.Timeout = new TimeSpan(0, 0, 0, 5);
+
+            Client = client;
+
+            return client;
+        }
+    }
+}
--- Yotify/ViewModel/PlaylistsViewModel.cs
+++ Yotify/ViewModel/PlaylistsViewModel.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Yotify.Core;
+using Yotify.Data.Model.Playlist;
 
 namespace Yotify.ViewModel
 {
     internal class PlaylistsViewModel: ObservableObject
     {
+        public RelayCommand PlaylistsCommand { get; set; }
+
+        public ObservableCollection<IPlaylist> Playlists { get; set; } 
+
+        public PlaylistsViewModel()
+        {
+            PlaylistsCommand = new RelayCommand(o =>
+            {
+                Debug.WriteLine("fetch playlists...");
+
+                if (Playlists == null)
+                {
+                    Playlists = new ObservableCollection<IPlaylist>();
+                }
+
+                Playlists.Add(new YoutubePlaylist("Playlist 1", "description 1", "http://playlist", "http://thumbnail"));
+                Playlists.Add(new YoutubePlaylist("Playlist 2", "description 2", "http://playlist", "http://thumbnail"));
+                Playlists.Add(new YoutubePlaylist("Playlist 3", "description 3", "http://playlist", "http://thumbnail"));
+                Playlists.Add(new YoutubePlaylist("Playlist 4", "description 4", "http://playlist", "http://thumbnail"));
+            });
+        }
     }
 }
